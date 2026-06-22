69a904211b1a386056662d646b8fb130183c96ae
Son Dao Dang <daodangson21052003@gmail.com>
2026-06-22 11:27:55 +0700

    ci: add backend unit tests to CI/CD pipeline

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const promClient = require('prom-client');
 const errorHandler = require('./middlewares/error.middleware');
 const routes = require('./routes/index');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// --- Prometheus metrics ---
+promClient.collectDefaultMetrics();
+
+const httpDuration = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status'],
+});
+
+app.use((req, res, next) => {
+  const end = httpDuration.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, status: res.statusCode });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
